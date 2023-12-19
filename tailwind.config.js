d9d24688a2010afce8f2ed6ac8303b34cc188828
Tazkia Athariza Dhivara <tazkiaatharizadhivara@gmail.com>
2023-12-19 21:59:21 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primary: "#73101E",
+        secondary: "#AB3C41",
+        tertiary:"#EF5B62",
+        pink:"#fCF5F3",
+        grey:"#2B2B2B",
+      }
+    },
+  },
+  plugins: [],
+}
+
